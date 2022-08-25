2a4bfd3338c3db35401e4f3d67f436908274aa77
shuaiwng <shuai.wang2@protonmail.com>
2022-08-25 20:11:59 +0200

    bumping rocket.

--- tutorial_SDL/main.cpp
+++ tutorial_SDL/main.cpp
@@ -1,22 +1,29 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.14159265
  
 int main(int argc, char ** argv)
 {
     bool quit = false;
     SDL_Event event;
+
+    Uint32 w_window = 640;
+    Uint32 h_window = 480;
  
     SDL_Init(SDL_INIT_VIDEO);
     IMG_Init(IMG_INIT_PNG);
  
     SDL_Window * window = SDL_CreateWindow("SDL2 Sprite Sheets",
-        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, w_window, h_window, 0);
     SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
     SDL_Surface * image = IMG_Load("./resource/rocket.png");
     SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer, image);
  
-  	SDL_SetRenderDrawColor(renderer, 50, 70, 150, 255);
+    SDL_SetRenderDrawColor(renderer, 50, 70, 150, 255);
 
     while (!quit)
     {
@@ -31,13 +38,16 @@ int main(int argc, char ** argv)
         }
 
         Uint32 ticks = SDL_GetTicks();	
-        Uint32 sprite = (ticks / 100) % 5;
+        Uint32 sprite = (ticks / 50) % 5;
+        Uint32 h_move_step = 10;
+        Uint32 h_move = (ticks / 30) % (w_window / h_move_step);
+        double rotate = 30.0*sin(sprite*PI/4);
 
         SDL_Rect srcrect = { sprite * 64, 0, 64, 64 };
-        SDL_Rect dstrect = { 10, 10, 64, 64 };
+        SDL_Rect dstrect = { h_move * h_move_step, h_window/2+int(50*sin(sprite*10*PI/180)), 64, 64 };
 
         SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, texture, &srcrect, &dstrect);
+        SDL_RenderCopyEx(renderer, texture, &srcrect, &dstrect, rotate, NULL, SDL_FLIP_NONE);
         SDL_RenderPresent(renderer);
     }
  
